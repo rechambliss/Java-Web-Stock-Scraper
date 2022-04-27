eead96d3985635da7ae278eaa4706ffb865faecf
Riley Chambliss <71612681+rechambliss@users.noreply.github.com>
2022-04-27 00:14:17 -0400

    Updated to fit NFL top 25 reciever stats

--- WebScrape.java
+++ WebScrape.java
@@ -1,47 +1,45 @@
 package webscrape;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 /**
  *
  * @author Shane - shanelee.co.uk
-*/
-
-
+ */
 public class WebScrape {
     public static void main(String[] args) {
-        final String url = 
-                "https://sharestobeclosed.telegraph.co.uk/indices/financials/index/MCX";
-        
+    int counter = 1;
+        final String url =
+
+                "https://www.nfl.com/stats/player-stats/category/receiving/2021/post/all/receivingreceptions/desc";
         try {
             final Document document = Jsoup.connect(url).get();
-            
             for (Element row : document.select(
-                "table.tablesorter.full tr")) {
+                    "table.d3-o-table.d3-o-table--detailed.d3-o-player-stats--detailed.d3-o-table--sortable tr")) {
                 if (row.select("td:nth-of-type(1)").text().equals("")) {
                     continue;
                 }
                 else {
-                   final String ticker = 
-                           row.select("td:nth-of-type(1)").text();
-                   final String name = 
-                           row.select("td:nth-of-type(2)").text();
-                   final String tempPrice = 
-                           row.select("td.right:nth-of-type(3)").text();
-                   final String tempPrice1 = 
-                           tempPrice.replace(",", "");
-//                    final double price = Double.parseDouble(tempPrice1);
-                   
-                    System.out.println(ticker + " " + name + " " + tempPrice1);
+                    final String name =
+                            row.select("td:nth-of-type(1)").text();
+                    final String rec =
+                            row.select("td:nth-of-type(2)").text();
+                    final String yds =
+                            row.select("td:nth-of-type(3)").text();
+                    final String tds =
+                            row.select("td:nth-of-type(4)").text();
+
+
+
+                    System.out.println(counter +".) Name: " + name+ " rec: " + rec + " yds: " + yds + " TDs: " + tds);
+                    counter++;
                 }
             }
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
     }
-    
+
 }
